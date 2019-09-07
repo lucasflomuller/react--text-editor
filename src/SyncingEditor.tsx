9a5da7014ec7463b0d080d1190ab166505526c7c
lucasflomuller <lucasflorianomuller@gmail.com>
2019-09-07 19:19:58 -0300

    add mitt for emit events

--- src/SyncingEditor.tsx
+++ src/SyncingEditor.tsx
@@ -1,13 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Editor } from 'slate-react'
+import Mitt from 'mitt';
 import { initialValue } from './slateInitialValue';
 
 interface Props {
 
 }
 
+const emitter = new Mitt();
+
 export const SyncingEditor: React.FC<Props> = () => {
   const [value, setValue] = useState(initialValue);
 
-  return <Editor value={value} onChange={(opts: any) => setValue(opts.value)} />
+  useEffect(() => {
+    emitter.on('*', () => {
+      console.log("change happened")
+    });
+  }, [])
+
+  return <Editor value={value} onChange={(opts: any) => {
+    setValue(opts.value)
+
+    const ops = opts.operations
+      .filter((o: any) => {
+        if (o) {
+          return (
+            o.type !== 'set_selection' &&
+            o.type !== 'set_value' &&
+            (!o.data || !o.data.has('source'))
+          )
+        }
+
+        return false;
+      }
+      )
+      .toJS()
+      .map((o: any) => ({ ...o, data: { source: "one" } }))
+
+    if (ops.length) {
+      emitter.emit('something', ops)
+    }
+  }}
+  />
 }
